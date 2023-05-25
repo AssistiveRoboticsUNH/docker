d5c6f44d8afee8c7c667081ff667adc08cb30317
olaghattas <olaghattas@hotmail.com>
2023-05-25 13:40:25 -0400

    launhc binary

--- smart-home/smart_home_launch.bash
+++ smart-home/smart_home_launch.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 source /home/smart-home/install/setup.bash
-# Launch the first "roslaunch" command in the background
-ros2 launch ros_tcp_endpoint endpoint.launch.py &
+
 sleep 1
-ros2 launch unity_launch launch_local.launch.py &
+ros2 launch unity_launch launch_pioneer.launch.py &
 sleep 1
 ros2 launch pioneer_navigation2 navigation2.launch.py &
 
