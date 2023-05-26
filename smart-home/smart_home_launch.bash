13986ccb04820d43c90c1efc13283c5b4bc9808c
olaghattas <olaghattas@hotmail.com>
2023-05-26 15:03:25 -0400

    work with jackal

--- smart-home/smart_home_launch.bash
+++ smart-home/smart_home_launch.bash
@@ -3,9 +3,9 @@
 source /home/smart-home/install/setup.bash
 
 sleep 1
-ros2 launch unity_launch launch_pioneer.launch.py &
+ros2 launch unity_launch launch_local_jackal.launch.py &
 sleep 1
-ros2 launch pioneer_navigation2 navigation2.launch.py &
+ros2 launch jackal_navigation navigation2.launch.py &
 
 # Wait for commands to finish
 wait
