e0de60a6d5457543e698d21e6c6084c414381bf5
Ola Ghattas <olaghattas@hotmail.com>
2023-05-31 00:17:14 -0400

    update

--- smart-home/smart_home_launch.bash
+++ smart-home/smart_home_launch.bash
@@ -3,7 +3,7 @@
 source /home/smart-home/install/setup.bash
 
 sleep 1
-ros2 launch unity_launch launch_local_jackal.launch.py &
+ros2 launch unity_launch launch_local_pioneer.launch.py &
 sleep 1
 ros2 launch jackal_navigation navigation2.launch.py &
 
