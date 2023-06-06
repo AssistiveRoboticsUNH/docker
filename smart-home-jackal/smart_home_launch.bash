f9cbdccf0d3d8cbd6ae31816b22458be714de134
olaghattas <olaghattas@hotmail.com>
2023-06-06 10:23:26 -0400

    update to support deep fake

--- smart-home-jackal/smart_home_launch.bash
+++ smart-home-jackal/smart_home_launch.bash
@@ -4,9 +4,9 @@ source /home/smart-home/install/setup.bash
 
 sleep 1
 ros2 launch shr_plan real_robot.launch.py &
+
 sleep 1
-## will be changed to ros2 launch pioneer_navigation navigation2.launch.py byththere is a bug so just use this for now it should work fine
-##ros2 launch pioneer_navigation navigation2.launch.py &
+ros2 launch shr_plan plansys2.launch.py &
 
 # Wait for commands to finish
 wait
--- smart-home/smart_home_launch.bash
+++ smart-home/smart_home_launch.bash
@@ -6,7 +6,7 @@ sleep 1
 ros2 launch unity_launch launch_local_pioneer.launch.py &
 sleep 1
 ## wiill be changed to ros2 launch pioneer_navigation navigation2.launch.py byththere is a bug so just use this for now it should work fine
-ros2 launch pioneer_navigation navigation2.launch.py &
+ros2 launch pioneer_navigation2 navigation2.launch.py &
 
 # Wait for commands to finish
 wait
